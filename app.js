6105f7fafb3a4342b2f091c21aa76bf3726e2a76
nikita-t1 <>
2022-04-23 08:57:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+const hostname = '127.0.0.1'
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.get('/foo/bar', (request, response) => {
+  response.send("foo")
+})
+
+app.get('/', (request, response) => {
+  response.send("Home")
+})
---
+++ index.ts
@@ -0,0 +1,34 @@
+import express, {Request,Response,Application} from 'express';
+
+const app:Application = express()
+const port = 3000
+const hostname = '127.0.0.1'
+
+require('dotenv').config();
+
+import { Client } from 'pg';
+const client = new Client()
+client.connect()
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.get('/foo/bar', (request, response) => {
+  response.send("foo")
+})
+
+app.get('/', (request, response) => {
+    response.send("Fuck Yeah")
+  })
+
+app.get('/api/times', (request:Request, response:Response) => {
+    client.query('SELECT * FROM public."time"', [], (err, res) => {
+        //console.log(res.rows.at(0))        
+        //console.log(res.rows.at(0).startTime)
+
+        client.end()
+        response.send(res.rows)
+
+    })
+})
